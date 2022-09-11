2b302c8d7fc44cf5fb1398f04be4a1374f71ecc4
LoiolaBr17 <mateustaua16@gmail.com>
2022-09-11 19:17:19 -0300

    criando o sistema de mensagens

---
+++ src/components/layout/Message.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+
+import styles from './Message.module.css'
+
+function Message({type, msg}){
+
+    const [visible, SetVisible] = useState(false)
+
+    useEffect(() => {
+        if(!msg){
+            SetVisible(false)
+            return
+        }
+
+        SetVisible(true)
+
+        const timer = setTimeout(() => {
+            SetVisible(false)
+        }, 3000)
+
+        return () => clearTimeout(timer)
+
+    }, [msg])
+
+    return (
+        <>
+        {visible && (
+            <div className={`${styles.message} ${styles[type]}`}>
+                {msg}
+            </div>
+        )}
+        </>
+    )
+}
+
+export default Message
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,20 @@
+import {useLocation} from 'react-router-dom'
+
+import Message from "../layout/Message"
+
 function Projects(){
+
+    const location = useLocation()
+    let message = ''
+    if(location.state) {
+        message = location.state.message
+    }
+
     return (
-        <>aqui</>
+        <div>
+            <h1>Meus Projetos</h1>
+            {message && <Message msg={message} type="success" />}
+        </div>
     )
 }
 
